5983ff911a50dd38f2abeedb1e4c148c763e89f6
aleca <aleca2002@yandex.ru>
2020-10-19 11:16:58 +0300

    commit 2

---
+++ WindowsFormsBoat/MotorBoat.cs
@@ -0,0 +1,73 @@
+﻿using System.Drawing;
+
+namespace WindowsFormsBoat
+{
+    public class MotorBoat : Boat
+    {
+        public Color DopColor { private set; get; }
+
+        /// <summary>      
+        /// Признак наличия боковых линий        
+        /// </summary>         
+        public bool SideLine { private set; get; }
+
+        // наличие кабины
+        public bool Cabin { private set; get; }
+
+        // наличие мотора
+        public bool Motors { private set; get; }
+
+        /// <summary>        
+        /// Конструктор         
+        /// </summary>         
+        /// <param name="maxSpeed">Максимальная скорость</param>         
+        /// <param name="weight">Вес лодки</param>         
+        /// <param name="mainColor">Основной цвет корпуса</param>        
+        /// <param name="dopColor">Дополнительный цвет (для полос) </param>                 
+        /// <param name="sideLine">Признак наличия боковых полос</param>   
+        /// <param name="cabin"><кабина катера/param>
+        /// <param name="motors">мотор </param>
+
+        public MotorBoat(int maxSpeed, float weight, Color mainColor, Color dopColor, bool sideLine, bool cabin, 
+            bool motors) : base (maxSpeed, weight, mainColor, 110, 60)
+        {
+            DopColor = dopColor;
+            SideLine = sideLine;
+            Cabin = cabin;
+            Motors = motors;
+        }
+
+        /// <summary>        
+        /// Отрисовка лодки      
+        /// /// </summary>        
+        /// <param name="g"></param>        
+        public override void DrawTransport(Graphics g)
+        {
+            base.DrawTransport(g);
+            //корпус
+            Brush brush1 = new SolidBrush(DopColor);
+            Pen pen1 = new Pen(Color.White);
+            if (Cabin)
+            {
+                //кабина катера
+                g.FillEllipse(brush1, _startPosX + 15, _startPosY - 3, 70, 15);
+                g.FillEllipse(brush1, _startPosX + 25, _startPosY - 1, 40, 10);
+            }
+            if (SideLine)
+            {
+                //линии
+                g.DrawLine(pen1, _startPosX - 10, _startPosY + 20, _startPosX + 100, _startPosY + 20);
+                g.DrawLine(pen1, _startPosX + 12, _startPosY, _startPosX + 70, _startPosY);
+            }
+
+            if (Motors)
+            {
+                //
+                Brush brush2 = new SolidBrush(Color.Black);
+                g.FillRectangle(brush2, _startPosX + 100, _startPosY + 10, 5, 5);
+                g.FillRectangle(brush2, _startPosX + 100, _startPosY + 16, 5, 5);
+                g.FillRectangle(brush2, _startPosX + 100, _startPosY + 22, 5, 5);
+            }
+        }
+    }
+}
---
+++ WindowsFormsBoat/Vehicle.cs
@@ -0,0 +1,55 @@
+﻿using System.Drawing;
+using WindowsFormsBoat;
+
+namespace WindowsFormsCars
+{
+    public abstract class Vehicle : ITransportBoat
+    {
+        /// <summary>        
+        /// Левая координата отрисовки лодки       
+        /// /// </summary>       
+        protected float _startPosX;
+
+        /// <summary>         
+        /// Правая кооридната отрисовки лодки       
+        /// </summary>         
+        protected float _startPosY;
+
+        /// <summary>         
+        /// Ширина окна отрисовки         /// </summary>        
+        protected int _pictureWidth;
+
+        /// <summary>        
+        /// Высота окна отрисовки         
+        /// </summary>         
+        protected int _pictureHeight;
+    
+        /// <summary>        
+        /// Максимальная скорость        
+        /// </summary>         
+        public int MaxSpeed { protected set; get; }
+
+        /// <summary>         
+        /// Вес лодки        
+        /// </summary>        
+
+        public float Weight { protected set; get; }
+
+        /// <summary>         
+        /// Основной цвет корпуса         
+        /// </summary>         
+        public Color MainColor { protected set; get; }
+
+
+        public void SetPosition(int x, int y, int width, int height)
+        {
+            _startPosX = x;
+            _startPosY = y;
+            _pictureWidth = width;
+            _pictureHeight = height;
+        }
+
+        public abstract void DrawTransport(Graphics g);
+        public abstract void MoveTransport(Direction direction);
+    }
+}
