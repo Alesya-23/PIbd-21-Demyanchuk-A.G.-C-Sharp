6fb35746158ead5fb8b08261c32ea120ae88ae13
Alesya-23 <52533375+Alesya-23@users.noreply.github.com>
2020-10-19 17:12:22 +0300

    Commit2

--- WindowsFormsBoat/Boat.cs
+++ WindowsFormsBoat/Boat.cs
@@ -1,70 +1,18 @@
-﻿using System.Drawing;
-
+using System.Drawing;
+using WindowsFormsCars;
 
 namespace WindowsFormsBoat
 {
-    class Boat
+    public class Boat : Vehicle
     {
-        /// <summary>        
-        /// Левая координата отрисовки лодки       
-        /// /// </summary>       
-        private float _startPosX;
-
-        /// <summary>         
-        /// Правая кооридната отрисовки лодки       
-        /// </summary>         
-        private float _startPosY;
-
-        /// <summary>         
-        /// Ширина окна отрисовки         /// </summary>        
-        private int _pictureWidth;
-
-        /// <summary>        
-        /// Высота окна отрисовки         
-        /// </summary>         
-        private int _pictureHeight;
         /// <summary>         
         /// Ширина отрисовки лодки         
         /// </summary> 
-        private readonly int boatWidth = 103;
+        protected readonly int boatWidth = 103;
         /// <summary>         
         /// Высота отрисовки лодки         
         /// </summary>         
-        private readonly int boatHeight = 42;
-
-
-        /// <summary>        
-        /// Максимальная скорость        
-        /// </summary>         
-        public int MaxSpeed { private set; get; }
-
-        /// <summary>         
-        /// Вес лодки        
-        /// </summary>        
-
-        public float Weight { private set; get; }
-
-        /// <summary>         
-        /// Основной цвет корпуса         
-        /// </summary>         
-        public Color MainColor { private set; get; }
-
-        /// <summary>         
-        ///Дополнительный цвет   (для полос)      
-        /// </summary>         
-        public Color DopColor { private set; get; }
-
-        /// <summary>      
-        /// Признак наличия боковых линий        
-        /// </summary>         
-        public bool SideLine { private set; get; }
-
-        // наличие кабины
-        public bool Cabin { private set; get; }
-
-        // наличие мотора
-        public bool Motors { private set; get; }
-
+        protected readonly int boatHeight = 42;
 
         /// <summary>        
         /// Конструктор         
@@ -72,42 +20,36 @@ namespace WindowsFormsBoat
         /// <param name="maxSpeed">Максимальная скорость</param>         
         /// <param name="weight">Вес лодки</param>         
         /// <param name="mainColor">Основной цвет корпуса</param>        
-        /// <param name="dopColor">Дополнительный цвет (для полос) </param>                 
-        /// <param name="sideLine">Признак наличия боковых полос</param>   
-        /// <param name="cabin"><кабина катера/param>
-        /// <param name="motors">мотор </param>
-
-        public Boat(int maxSpeed, float weight, Color mainColor, Color dopColor, bool sideLine, bool cabin, bool motors)
+        public Boat(int maxSpeed, float weight, Color mainColor)
         {
             MaxSpeed = maxSpeed;
             Weight = weight;
             MainColor = mainColor;
-            DopColor = dopColor;
-            SideLine = sideLine;
-            Cabin = cabin;
-            Motors = motors;
         }
 
-        /// <summary>         
-        /// Установка позиции лодки
-        /// </summary>      
-        /// <param name="x">Координата X</param>        
-        /// <param name="y">Координата Y</param>         
-        /// <param name="width">Ширина картинки</param>        
-        /// <param name="height">Высота картинки</param>        
-        public void SetPosition(int x, int y, int width, int height)
+        /// <summary>
+        /// Конструкторс изменением размеров машины
+        /// </summary>
+        /// <param name="maxSpeed">Максимальная скорость</param>
+        /// <param name="weight">Вес лодки</param>
+        /// <param name="mainColor">Основной цвет корпуса лодки</param>
+        /// <param name="boatWidth">Ширина отрисовки лодки</param>
+        /// <param name="boatHeight">Высота отрисовки лодки</param>
+        protected Boat(int maxSpeed, float weight, Color mainColor, int boatWidth, int
+            boatHeight)
         {
-            _startPosX = x;
-            _startPosY = y;
-            _pictureWidth = width;
-            _pictureHeight = height;
+            MaxSpeed = maxSpeed;
+            Weight = weight;
+            MainColor = mainColor;
+            this.boatWidth = boatWidth;
+            this.boatHeight = boatHeight;
         }
 
         /// <summary>         
         ///Изменение направления пермещения         
         /// </summary>        
         /// <param name="direction">Направление</param>         
-        public void MoveTransport(Direction direction)
+        public override void MoveTransport(Direction direction)
         {
             float step = MaxSpeed * 100 / Weight;
             switch (direction)
@@ -149,37 +91,14 @@ namespace WindowsFormsBoat
         /// Отрисовка лодки      
         /// /// </summary>        
         /// <param name="g"></param>        
-        public void DrawTransport(Graphics g)
+        public override void DrawTransport(Graphics g)
         {
-            Pen pen = new Pen(Color.Black);
             //корпус
             Brush brush = new SolidBrush(MainColor);
-            Brush brush1 = new SolidBrush(DopColor);
             Pen pen1 = new Pen(Color.White);
             g.FillEllipse(brush, _startPosX, _startPosY, 100, 35);
-            g.DrawLine(pen1, _startPosX + 12, _startPosY, _startPosX + 70, _startPosY);
-            if (Cabin)
-            {
-                //кабина катера
-                g.FillEllipse(brush1, _startPosX + 15, _startPosY - 3, 70, 15);
-                g.FillEllipse(brush1, _startPosX + 25, _startPosY - 1, 40, 10);
-            }
-            if (SideLine)
-            {
-                //линии
-                g.DrawLine(pen1, _startPosX - 10, _startPosY + 20, _startPosX + 100, _startPosY + 20);
-                g.DrawLine(pen1, _startPosX - 10, _startPosY + 22, _startPosX + 100, _startPosY + 22);
-                g.DrawLine(pen1, _startPosX + 12, _startPosY, _startPosX + 70, _startPosY);
-            }
+            g.DrawLine(pen1, _startPosX - 10, _startPosY + 22, _startPosX + 100, _startPosY + 22);
 
-            if (Motors)
-            {
-                //
-                Brush brush2 = new SolidBrush(Color.Black);
-                g.FillRectangle(brush2, _startPosX + 100, _startPosY + 10, 5, 5);
-                g.FillRectangle(brush2, _startPosX + 100, _startPosY + 16, 5, 5);
-                g.FillRectangle(brush2, _startPosX + 100, _startPosY + 22, 5, 5);
-            }
         }
     }
 }
